e082bf0623c6045feb7acd7c75e775676289e9bf
shrinik <24953626+shrinik@users.noreply.github.com>
2024-05-10 23:15:22 +0530

    Added new Hi component

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import pluginVue from 'eslint-plugin-vue'
 export default [
   // add more generic rulesets here, such as:
   // js.configs.recommended,
-  ...pluginVue.configs['flat/recommended'],
+  ...pluginVue.configs['flat/essential'],
   // ...pluginVue.configs['flat/vue2-recommended'], // Use this if you are using Vue.js 2.x.
   {
     rules: {
